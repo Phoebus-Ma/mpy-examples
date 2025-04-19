deb614301f3e9c2219c846bfe6b57e4e4488a621
Phoebus <tianjindahai@126.com>
2025-04-19 17:47:05 +0800

    [esp] Add read i2c address example.

---
+++ esp/i2c/scan_addr/main.py
@@ -0,0 +1,23 @@
+###
+# Read i2c address example.
+#
+# License - MIT.
+###
+
+# 读取i2c从设备的地址, 适合在不知道设备地址等情况.
+from machine import I2C, Pin
+
+
+# 初始化I2C总线，使用默认的SCL和SDA引脚 (PIO22和GPIO21).
+i2c = I2C(scl=Pin(22), sda=Pin(21))
+
+# 扫描I2C总线以检测连接的设备.
+devices = i2c.scan()
+
+# 打印检测到的设备地址.
+if devices:
+    print("Detect i2c address:")
+    for device in devices:
+        print(hex(device))
+else:
+    print("Not found i2c device.")
